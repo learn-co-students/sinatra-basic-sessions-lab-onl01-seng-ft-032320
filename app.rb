1f600e12fff307cf61d5a2940832ec9d7569e28d
antdp425 <antdp425@aol.com>
2020-04-30 16:38:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+   configure do
+      enable :sessions
+      set :session_secret, "th!$se$$ion"
+   end
+
+   get '/' do
+      erb :index
+   end
+
+   get '/checkout' do
+      erb :shows
+   end
+
+   post '/checkout' do
+      session[:item] = params[:item]
+      @session = session
+   redirect to "/checkout"
+   end
 end
