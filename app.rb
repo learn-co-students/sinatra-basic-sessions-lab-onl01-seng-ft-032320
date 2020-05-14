b15e46d56077bb2e40ac43ba00c577280504b586
Kevin Mathias <komathias1@gmail.com>
2020-05-14 11:21:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret" #the string can be whatever you want, like a 'password' I guess? So not secret, haha.
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do 
+    @sessions = session #adding the params from the form to the session hash?
+    @sessions[:item] = params["item"] #Not quit sure whats going on here, setting the items params to the items key in the hash?
+  end
 end
